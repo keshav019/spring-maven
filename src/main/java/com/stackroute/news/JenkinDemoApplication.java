1ba10e1b9842a123ffd4065ee8ecb6e7caee4f7a
keshav019 <keshavkumaryadav2000@gmail.com>
2023-08-20 18:17:58 +0530

    second Commit

---
+++ src/main/java/com/stackroute/news/JenkinDemoApplication.java
@@ -0,0 +1,20 @@
+package com.stackroute.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Welocome to JenkinDemo";
+	}
+}
---
+++ src/test/java/com/stackroute/news/JenkinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
